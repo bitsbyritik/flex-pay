867548ac9c3c6f0465866731048a867520bfc233
bitsbyritik <bitsbyritik@gmail.com>
2025-02-26 22:12:55 +0530

    added_sol_adapter

---
+++ components/SolanaWalletProvider.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { FC, ReactNode, useMemo } from "react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { clusterApiUrl } from "@solana/web3.js";
+import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+
+interface Props {
+  children: ReactNode;
+}
+
+const SolanaWalletProvider: FC<Props> = ({ children }) => {
+  const network = WalletAdapterNetwork.Devnet;
+
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(() => [], [network]);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default SolanaWalletProvider;
